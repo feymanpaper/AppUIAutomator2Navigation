873490c865cd9e184759900e2c4bc29b73da0c35
+v <92511471+MayBiscuit@users.noreply.github.com>
2023-08-26 16:28:09 +0800

    画跳转图 (#6)
    * 修改路径
    
    * 修改路径
    
    * 修改格式

--- Utils/DrawGraphUtils.py
+++ Utils/DrawGraphUtils.py
@@ -18,26 +18,171 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.patches import ConnectionPatch
 from PIL import Image
-
-# 点布局相关
-x_root = 0
-y_root = 0
-x_space = 3000
-y_space = 6000
-
-# 截图比例相关
-node_size = 1000
-length_to_width_ratio = 1.5
+from ScreenshotUtils import *
 
 class DrawGraphUtils:
+
     @staticmethod
     def draw_callgraph(jsonFilePath, screenShotFilePath, svgSaveFilePath):
-        pass
+        result = DrawGraphUtils.load_data(jsonFilePath)
+        num_objects, graph, pos = result
+
+        # 获取截图
+        screenshot_folder = os.path.abspath(screenShotFilePath)
+        screenshot_files = {node: os.path.join(screenshot_folder, f'{ScreenshotUtils.encode_screen_uid(node)}.png') for node in graph.nodes}
+        nx.set_node_attributes(graph, screenshot_files, 'image')
+
+        # 画图
+        fig, ax = plt.subplots(figsize=(100, 150))
+        plt.axis('off')
+
+        node_size = 1000
+        length_to_width_ratio = 1.5
+
+        # 调整截图位置和比例
+        for node in graph.nodes:
+            x, y = pos[node]
+            img_path = graph.nodes[node]['image']
+            img = Image.open(img_path)
+            resized_img = img.resize((node_size, int(node_size * length_to_width_ratio)))
+            img_width, img_height = img.size
+
+            x -= img_width / 2
+            y -= img_height / 2
+            ax.imshow(resized_img, extent=(x, x + img_width, y, y + img_height), zorder=2, interpolation='nearest')
+
+        # 画边
+        nx.draw_networkx_edges(graph, pos, ax=ax, width=1.5, edge_color='gray', arrows=False, alpha=0.1)
+        for edge in graph.edges():
+            start_node = edge[0]
+            end_node = edge[1]
+            x_start, y_start = pos[start_node]
+            x_end, y_end = pos[end_node]
+            arrow_pos_x = (x_start + x_end) / 2
+            arrow_pos_y = (y_start + y_end) / 2
+
+            cl = graph.edges[edge]['color']
+            l_w = -0.015 * num_objects + 5.5
+            m_s = -0.25 * num_objects + 85
+
+            arrow_connection = ConnectionPatch((x_start, y_start), (arrow_pos_x, arrow_pos_y),
+                                               "data", "data", arrowstyle='->', color=cl, alpha=0.8, linewidth=l_w, mutation_scale=m_s)
+            next_connection = ConnectionPatch((arrow_pos_x, arrow_pos_y), (x_end, y_end),
+                                              "data", "data", arrowstyle='-', color=cl, alpha=0.8, linewidth=l_w, mutation_scale=m_s)
+            ax.add_artist(arrow_connection)
+            ax.add_artist(next_connection)
+
+        DrawGraphUtils.save_data(jsonFilePath, svgSaveFilePath)
 
     @staticmethod
-    def load_data():
-        pass
+    def load_data(jsonFilePath):
+        # 读取json文件，返回界面数量、跳转关系和画图位置
+        json_file_path = os.path.abspath(jsonFilePath)
+
+        with open(json_file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            num_objects = len(data)
+
+            graph = nx.DiGraph()
+            pos = {}
+
+            record_pos = {}
+            x_root = 0
+            y_root = 0
+            x_space = 3000
+            y_space = 6000
+            virtual_root_flag = 1
+            root_flag = 1
+            root_count = 0
+            edge_color = 'orange'
+
+            for obj in data:
+                ck_eles_text = obj["ck_eles_text"]
+                nextlist = obj["nextlist"]
+                call_map = obj["call_map"]
+
+                # 虚拟根节点
+                if virtual_root_flag:
+                    virtual_root_flag = 0
+                    continue
+
+                # 初始根节点
+                if root_flag and not virtual_root_flag:
+                    graph.add_node(ck_eles_text)
+                    pos[ck_eles_text] = (x_root, y_root)
+                    record_pos[y_root] = x_root
+                    root_flag = 0
+
+                # 遍历完一支后新的根节点
+                if not graph.has_node(ck_eles_text):
+                    graph.add_node(ck_eles_text)
+                    root_count += 1
+
+                    if len(nextlist) > 0 and graph.has_node(nextlist[0]) and nextlist[0] in pos:
+                        _, y_nxt = pos[nextlist[0]]
+                        # y:放置在后续第一个子节点的上方y_space/2处
+                        y_new = y_nxt + y_space / 2
+                        # x:放置在y高度线上最右边节点的旁边
+                        x_record = [x for y, x in record_pos.items() if y == y_nxt]
+                        if len(x_record) > 0:
+                            max_x_record = max(x_record)
+                            x_new = max_x_record + x_space
+                        else:
+                            x_new = x_root + root_count * x_space
+
+                        record_pos[y_nxt] = x_new
+                    else:
+                        y_new = y_root
+
+                        x_record = [x for y, x in record_pos.items() if y == y_root]
+                        if len(x_record) > 0:
+                            max_x_record = max(x_record)
+                            x_new = max_x_record + x_space
+
+                        record_pos[y_root] = x_new
+
+                    pos[ck_eles_text] = (x_new, y_new)
+
+                x_parent, y_parent = pos[ck_eles_text]
+                num = len(nextlist)
+                x_first = x_parent - num * x_space / 2
+                order = 0
+
+                if edge_color == 'orange':
+                    edge_color = 'cyan'
+                else:
+                    edge_color = 'orange'
+
+                # 子节点
+                for nxt in nextlist:
+                    if not graph.has_node(nxt):
+                        graph.add_node(nxt)
+                        # x: 在父节点正下方散开
+                        x_child = x_first + order * x_space
+                        order += 1
+                        # y：放在父节点下方y_space处
+                        y_child = y_parent - y_space
+
+                        pos[nxt] = (x_child, y_child)
+                        record_pos[y_child] = x_child
+
+                    if nxt is not None and ck_eles_text != nxt:
+                        graph.add_edge(ck_eles_text, nxt, color=edge_color)
+
+                for _, target in call_map.items():
+                    if target is not None and ck_eles_text != target:
+                        graph.add_edge(ck_eles_text, target, color=edge_color)
+
+        return num_objects, graph, pos
 
     @staticmethod
-    def save_data():
-        pass
+    def save_data(jsonFilePath, svgSaveFilePath):
+        # 保存svg图像，图像与json文件同名
+        svg_save_folder = os.path.abspath(svgSaveFilePath)
+
+        if not os.path.exists(svg_save_folder):
+            os.makedirs(svg_save_folder)
+
+        svg_save_name = jsonFilePath.replace("./dumpjson", "")
+        svg_save_name = svg_save_name.replace(".json", ".svg")
+        plt.savefig(svg_save_folder + svg_save_name, bbox_inches='tight', dpi=300)
--- Utils/test/DrawGraphUtils_test.py
+++ Utils/test/DrawGraphUtils_test.py
@@ -3,8 +3,14 @@ from Utils.DrawGraphUtils import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)
-        DrawGraphUtils.draw_callgraph()
+        # TODO
+        jsonFilePath = "./dumpjson/com.alibaba.android.rimet_restart0activity4&screen17&time177.74s.json"
+        # TODO
+        screenShotFilePath = "./Screenshot/ScreenshotPicture"
+        # TODO 将svg文件保存到目标文件夹，文件夹不存在直接创建
+        svgSaveFilePath = "./svgSaveDir"
+
+        DrawGraphUtils.draw_callgraph(jsonFilePath, screenShotFilePath, svgSaveFilePath)
 
 if __name__ == '__main__':
     unittest.main()
