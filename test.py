089d0b42ba4593b62ad93e09d5b5b855be75ee10
feymanpaper <feymanpaper@gmail.com>
2023-08-26 16:37:51 +0800

    what

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ from Utils.DrawGraphUtils import *
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         #TODO
-        jsonFilePath = "./dumpjson/com.alibaba.android.rimet_restart2activity9&screen39&time504.7s.json"
-        #TODO
-        screenShotFilePath = "./ScreenshotPicture"
-        #TODO 将svg文件保存到目标文件夹，文件夹不存在直接创建
+        jsonFilePath = "./dumpjson/com.alibaba.android.rimet_restart0activity4&screen17&time177.74s.json"
+        # TODO
+        screenShotFilePath = "./Screenshot/ScreenshotPicture"
+        # TODO 将svg文件保存到目标文件夹，文件夹不存在直接创建
         svgSaveFilePath = "./svgSaveDir"
-        DrawGraphUtils.draw_callgraph()
+
+        DrawGraphUtils.draw_callgraph(jsonFilePath, screenShotFilePath, svgSaveFilePath)
 
 
 if __name__ == '__main__':
