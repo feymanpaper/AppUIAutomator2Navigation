a05eeff6578194071d5e6b244a90921b2aa8463a
MayBisCuit <jiawei03@qq.com>
2023-08-30 21:13:32 +0800

    修改传入参数为包名时间字符串

--- Utils/DrawGraphUtils_test.py
+++ Utils/DrawGraphUtils_test.py
@@ -3,8 +3,10 @@ from Utils.DrawGraphUtils import *
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
+
         package = "com.alibaba.android.rimet-20230830-173908"
         DrawGraphUtils.draw_callgraph(package)
 
 if __name__ == '__main__':
+
     unittest.main()
