72d08384e66f4bd967db1c0dfb64cc5da66bf234
feymanpaper <feymanpaper@gmail.com>
2023-08-30 14:11:37 +0800

    rebase resolve conflict

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         #TODO
         jsonFilePath = "./dumpjson/com.alibaba.android.rimet_restart0activity4&screen9&time85.02s.json"
+
         # TODO
         screenShotFilePath = "./Screenshot/ScreenshotPicture"
         # TODO 将svg文件保存到目标文件夹，文件夹不存在直接创建
