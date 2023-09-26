52c4ef2003e885629551b83448e650d0bee34203
codingman-aiwass <abc1277046286@gmail.com>
2023-09-26 14:30:43 +0800

    弃用了run.sh run.ps1

--- test_integrate.py
+++ test_integrate.py
@@ -25,25 +25,29 @@ def execute_cmd_with_timeout(cmd, timeout=600):
         p.send_signal(signal.SIGINT)
         p.wait()
 
+if __name__ == '__main__':
+    config_settings = {'ui_static': 'true', 'ui_dynamic': 'true', 'code_inspection': 'true',
+                           'pp_print_permission_info': 'true', 'pp_print_sdk_info': 'true',
+                           'pp_print_sensitive_item': 'true', 'pp_print_others': 'true', 'pp_print_long_sentences': 'true',
+                           'dynamic_print_full_ui_content': 'true', 'dynamic_print_sensitive_item': 'true',
+                           'get_pp_from_app_store': 'true', 'get_pp_from_dynamically_running_app': 'false',
+                           'dynamic_ui_depth': '3', 'dynamic_pp_parsing': 'true'}
 
-config_settings = {'ui_static': 'true', 'ui_dynamic': 'true', 'code_inspection': 'true',
-                       'pp_print_permission_info': 'true', 'pp_print_sdk_info': 'true',
-                       'pp_print_sensitive_item': 'true', 'pp_print_others': 'true', 'pp_print_long_sentences': 'true',
-                       'dynamic_print_full_ui_content': 'true', 'dynamic_print_sensitive_item': 'true',
-                       'get_pp_from_app_store': 'true', 'get_pp_from_dynamically_running_app': 'false',
-                       'dynamic_ui_depth': '3', 'dynamic_pp_parsing': 'true'}
-
-with open('apk_pkgName.txt') as f:
-    content = f.readlines()
-pkgName_appName_list = [item.rstrip('\n') for item in content]
-for pkgName_appName in pkgName_appName_list:
-    try:
-        pkgName, appName = pkgName_appName.split(' | ')
-        appName = appName.strip('\'')
-        if get_OS_type() in ['linux', 'mac']:
-            execute_cmd_with_timeout('./run.sh {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
-        elif get_OS_type() == 'win':
-            execute_cmd_with_timeout(
-                'PowerShell.exe ./run.ps1 {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
-    except Exception:
-        print('error occurred, continue...')
+    with open('apk_pkgName.txt') as f:
+        content = f.readlines()
+    pkgName_appName_list = [item.rstrip('\n') for item in content]
+    for pkgName_appName in pkgName_appName_list:
+        if pkgName_appName.startswith('# '):
+            continue
+        try:
+            pkgName, appName = pkgName_appName.split(' | ')
+            appName = appName.strip('\'')
+            if get_OS_type() in ['linux', 'mac']:
+                execute_cmd_with_timeout('python3 run.py {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
+                # execute_cmd_with_timeout('./run.sh {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
+            elif get_OS_type() == 'win':
+                execute_cmd_with_timeout('python run.py {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
+                # execute_cmd_with_timeout(
+                #     'PowerShell.exe ./run.ps1 {} {} {}'.format(pkgName, appName, config_settings['dynamic_ui_depth']))
+        except Exception:
+            print('error occurred, continue...')
