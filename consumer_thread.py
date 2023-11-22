dbe64359efc10c8d17f3dcf43d779268cffba6cd
codingman-aiwass <abc1277046286@gmail.com>
2023-11-22 21:41:50 +0800

    修复了守护线程获取隐私政策URL文件时因为该文件尚未写入导致的bug

--- consumer_thread.py
+++ consumer_thread.py
@@ -25,7 +25,7 @@ def consumer_thread(queue):
         print('consumer thread waiting for data...')
         data = queue.get()
         print("Processing data:", data)
-        # 缓冲5s，等待写入
+        # 缓冲5s，等待隐私政策URL被写入
         time.sleep(5)
         # 进行相应的处理操作
         pp_url_path, pkgName_appName = data.split('||')
@@ -36,29 +36,38 @@ def consumer_thread(queue):
             continue
         # 在这里进行其他处理操作
         app_pp = {}
-        with open(pp_url_path, 'r', encoding='utf-8') as f:
-            content = f.readlines()
-            content = [item.strip('\n') for item in content]
-            print('content in txt file of PrivacyPolicy', content)
-            pp_url = content
-            print('pp_url:', pp_url)
-            if len(pp_url) == 1:
-                if 'html' in pp_url[0]:
-                    app_pp[pkgName] = [pp_url[0][:pp_url[0].index('html') + 4]][:]
-                elif 'htm' in pp_url[0]:
-                    app_pp[pkgName] = [pp_url[0][:pp_url[0].index('htm') + 3]][:]
-                else:
-                    if pp_url[0].endswith('.1.1'):
-                        # 只有这一个结果，还是不合格的，视为没找到隐私政策
-                        print('privacy policy not in ', pkgName)
-                        pp_urls, missing_urls = get_pp_from_app_store(
-                            get_pkg_names_from_input_list([pkgName]))
-                        app_pp.update(pp_urls)
-                    else:
-                        app_pp[pkgName] = pp_url[:]
+        flag = True
+        while flag:
+            try:
+                with open(pp_url_path, 'r', encoding='utf-8') as f:
+                    content = f.readlines()
+                    content = [item.strip('\n') for item in content]
+                    print('content in txt file of PrivacyPolicy', content)
+                    flag = False
+            except FileNotFoundError:
+                # 说明还没有写入文件系统
+                time.sleep(5)
+                print('try again.')
 
-            elif len(pp_url) > 1:
-                app_pp[pkgName] = pp_url[:]
+        pp_url = content
+        print('pp_url:', pp_url)
+        if len(pp_url) == 1:
+            if 'html' in pp_url[0]:
+                app_pp[pkgName] = [pp_url[0][:pp_url[0].index('html') + 4]][:]
+            elif 'htm' in pp_url[0]:
+                app_pp[pkgName] = [pp_url[0][:pp_url[0].index('htm') + 3]][:]
+            else:
+                if pp_url[0].endswith('.1.1'):
+                    # 只有这一个结果，还是不合格的，视为没找到隐私政策
+                    print('privacy policy not in ', pkgName)
+                    pp_urls, missing_urls = get_pp_from_app_store(
+                        get_pkg_names_from_input_list([pkgName]))
+                    app_pp.update(pp_urls)
+                else:
+                    app_pp[pkgName] = pp_url[:]
+
+        elif len(pp_url) > 1:
+            app_pp[pkgName] = pp_url[:]
         # 将最终输出给隐私政策分析模块的文件修改为 包名:[应用名，[url列表]]的形式
         if type(app_pp[pkgName]) == list:
             app_pp[pkgName] = [appName, app_pp[pkgName][:]]
@@ -72,15 +81,13 @@ def consumer_thread(queue):
         os_type = get_OS_type()
         print('call pp analysis module in consumer_thread!')
         if os_type == 'win':
-            subprocess.run(['python', 'privacy-policy-main.py'],
+            subprocess.run(['python', 'privacy-policy-main.py','y'],
                            cwd=os.path.join('..', 'Privacy-compliance-detection-2.1', 'core'),
                            timeout=600)
         elif os_type in ['linux', 'mac']:
-            subprocess.run(['python3', 'privacy-policy-main.py'],
+            subprocess.run(['python3', 'privacy-policy-main.py','y'],
                            cwd=os.path.join('..', 'Privacy-compliance-detection-2.1', 'core'),
                            timeout=600)
-        # TODO 需要加一个检测机制,判断这个应用的隐私政策到底解析成功没有.没有成功的话,还得靠原本的逻辑.
-        #  逻辑可以用最简单的,看PrivacyPolicySaveDir里有没有这个应用的json解析结果.
         files_in_privacy_policy_save_dir = os.listdir(
             os.path.join('..', 'Privacy-compliance-detection-2.1', 'core', 'PrivacyPolicySaveDir'))
         if pkgName + '.json' in files_in_privacy_policy_save_dir and pkgName + '_sdk.json' in files_in_privacy_policy_save_dir:
