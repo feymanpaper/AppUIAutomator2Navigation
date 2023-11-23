c0c5712da0f80641559146876764bdd35be53428
feymanpaper <feymanpaper@gmail.com>
2023-11-23 21:39:22 +0800

    fix: change click sleep time to 500ms and add retry

--- Config.py
+++ Config.py
@@ -60,7 +60,7 @@ class Config(object):
 
         self.curDepth = 1
         self.test_time = int(test_time)  # 配置测试的时间,以秒为单位
-        self.sleep_time_sec = 0.2  # 配置点击之后睡眠的时间
+        self.sleep_time_sec = 0.5  # 配置点击之后睡眠的时间
 
         self.isSearchPrivacyPolicy = False # 配置是否寻找隐私政策
         self.isDrawAppCallGraph = False  # 配置是否绘制App界面跳转图
