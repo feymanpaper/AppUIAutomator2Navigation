e0d3ef2c6eeb943da88ea7cd9679f797c24d2775
6yaozt <6yaozt@users.noreply.github.com>
2023-12-11 22:55:16 +0800

    mislead_change

--- services/mislead_detector/check_is_mislead.py
+++ services/mislead_detector/check_is_mislead.py
@@ -1,9 +1,18 @@
-# import your model from your_other_pkg
+import requests
 
-def check_is_mislead_text(text :str) -> bool:
+def check_is_mislead_text(text: str) -> bool:
     """
     Check if the input text is misleading text
-    :param text
-    :return: True or False
+    :param text: The input text to be checked
+    :return: True if the text is misleading, False otherwise
     """
-    pass
+    api_url = 'http://localhost:5000/check_misleading'  # Replace with the actual URL where the API is hosted
+    data = {'text': text}
+    response = requests.post(api_url, json=data)
+
+    if response.status_code == 200:
+        result = response.json()
+        return result['is_misleading']
+    else:
+        print("Failed to call the API:", response.status_code, response.text)
+        return False  # Return False if there was an error calling the API
--- services/mislead_detector/save_mislead_file.py
+++ services/mislead_detector/save_mislead_file.py
@@ -1,15 +1,45 @@
+import os
+import shutil
+from datetime import datetime
+import random
 
-def save_mislead_file(abs_path:str, from_img:str, to_img:str, click_xy:tuple) -> str:
-    """
-    Save mislead file (including from_img, to_img and clicked widget coordinates) to `mislead_res`
-    return: directory where files are saved
-    demo_case:
-        input:
-            abs_path: collectData\com.alibaba.aliyun-20231202-003938
-            from_img: 0IfqneiOmh6xX4VulvUwX41EwIHFKt4C7TNY3BWX53o=.png
-            to_img: 0UGIAyuB3G077ow6mMUnRMnQyYLdmz2ZHFhLjUi1prY=.png
-            click_xy: (255, 255)
-        output:
-            the mislead file saved in `mislead_res`
-    """
-    pass
+def get_random_str():
+    str = ''.join(random.sample(
+        ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e',
+         'd', 'c', 'b', 'a'], 10))
+    return str
+
+def save_mislead_file(abs_path: str, from_img: str, to_img: str, click_xy: tuple) -> str:
+    # 创建文件夹名,获取包名
+    # package_name = abs_path.split("\\")[-1].split("-")[0]
+    parts = abs_path.split("\\")
+    package_name = parts[1].split("-")[0]
+    # 加上时间戳
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    folder_name = f"{package_name}_{timestamp}"
+
+    # 创建文件夹,在某个路径下，这里写的绝对路径
+    base_directory = "C:/Codelife/ui/collectData/mislead_data"
+    directory = os.path.join(base_directory, folder_name)
+    os.makedirs(directory)
+
+    # 保存图片
+    from_name=get_random_str()
+    to_name=get_random_str()
+    from_img_path = os.path.join(directory, f"{from_name}.png")
+    to_img_path = os.path.join(directory, f"{to_name}.png")
+    # 复制图片到新路径,使用绝对路径
+    abs_path = "collectData\com.alibaba.aliyun-20231202-003938\Screenshot\ScreenshotPicture"
+    abs_path = abs_path.replace("\\", "/")
+    abs_path = "C:/Codelife/ui/" + abs_path
+    from_img_full_path = os.path.join(abs_path, from_img)
+    to_img_full_path = os.path.join(abs_path, to_img)
+    shutil.copy(from_img_full_path, from_img_path)
+    shutil.copy(to_img_full_path, to_img_path)
+
+    # 保存点击坐标到txt文件
+    with open(os.path.join(directory, "ui_relation.txt"), "w") as file:
+        file.write(f"{to_name} {from_name} {click_xy[0]} {click_xy[1]}")
+
+
+    return directory
